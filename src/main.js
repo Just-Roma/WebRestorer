7acc78211ec31277a7656862086231e5bd673ecd
Roman Swietlicki <64587275+Just-Roma@users.noreply.github.com>
2024-07-14 22:10:29 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -73,90 +73,176 @@ const html_style = getComputedStyle(html);
           Properties handling
  *------------------------------------*
 */
-function handle_overflow(element, element_style){
 
-  let overflow = element_style['overflow'];
-  let position = element_style['position'];
 
-  if (overflow.includes('hidden')){
-    element.style.setProperty('overflow', overflow.replace('hidden', 'auto'), 'important');
+const special_elements = {
+  /*
+  Holder for different groups of HTML elements.
+  */
+  excluded_for_transparency: [
     /*
-    Check its 'position' property:
-    - If it is 'fixed', then it will still be unmovable.
+    For some elements transparency can be a part of design, it would be nice to preserve it.
+    It is highly unlikely/impossible that these elements would be used as containers for popups.
     */
-    if (position.includes('fixed')){
-      element.style.setProperty('position', position.replace('fixed', 'static'), 'important');
+    'A', 'ABBR', 'ACRONYM', 'ADDRESS', 'B', 'BDI', 'BDO', 'BLOCKQUOTE', 'BUTTON', 'CAPTION', 'CITE', 'CODE', 'COL', 'COLGROUP',
+    'DATA', 'DD', 'DEL', 'DETAILS', 'DFN', 'DL', 'DT', 'EM', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FORM', 'H1', 'H2', 'H3', 'H4',
+    'HGROUP', 'HR', 'I', 'INPUT', 'INS', 'KBD', 'LABEL', 'LEGEND', 'LI', 'MARK', 'METER', 'OL', 'OPTGROUP', 'OPTION', 'OUTPUT',
+    'P', 'PRE', 'PROGRESS', 'Q', 'RB', 'RP', 'RT', 'RTC', 'RUBY', 'S', 'SAMP', 'SEARCH', 'SELECT', 'SMALL', 'SPAN', 'STRONG',
+    'SUB', 'SUMMARY', 'SUP', 'TABLE', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TR', 'TT', 'U', 'UL', 'VAR',
+  ],
+
+  excluded_for_z_index: [
+    /*
+    For some elements transparency can be a part of design, it would be nice to preserve it.
+    It is highly unlikely/impossible that these elements would be used as containers for popups.
+    */
+    'A', 'ARTICLE', 'ASIDE', 'BUTTON', 'CANVAS', 'FOOTER', 'FORM', 'FRAME', 'FRAMESET',
+    'H1', 'H2', 'H3', 'H4', 'HEADER', 'IFRAME', 'MAIN', 'NAV', 'P', 'SPAN',
+  ],
+
+  ancestors_for_z_index: [
+    /*
+    These elements are special and are highly unlikely to be a container for an element with numeric z-index.
+    */
+    'A', 'ARTICLE', 'ASIDE', 'BUTTON', 'CANVAS', 'FOOTER', 'FORM', 'FRAME', 'FRAMESET',
+    'H1', 'H2', 'H3', 'H4', 'HEADER', 'IFRAME', 'MAIN', 'NAV', 'P', 'SPAN',
+  ],
+
+  descendants_for_z_index: [
+    /*
+    These elements are special and are highly unlikely to be within an element with numeric z-index.
+    */
+    'HEADER', 'MAIN', 'ARTICLE', 'ASIDE', 'NAV', 'FOOTER',
+  ],
+};
+
+
+const css_properties_handlers = {
+  /*
+  Wrapper for functions, which are used to handle annoyances-related CSS properties.
+  */
+  handle_overflow: function (element, element_style){
+    /*
+    Overflow is sometimes restricted in order to prevent users from scrolling content.
+    */
+    let overflow = element_style['overflow'];
+    let position = element_style['position'];
+
+    if (overflow.includes('hidden')){
+      element.style.setProperty('overflow', overflow.replace('hidden', 'auto'), 'important');
+      /*
+      Check its 'position' property:
+      - If it is 'fixed', then it will still be unmovable.
+      */
+      if (position.includes('fixed')) element.style.setProperty('position', position.replace('fixed', 'static'), 'important');
     }
-  }
-}
+  },
 
-function handle_blur(element, element_style){
+  handle_blur: function (element, element_style){
+    /*
+    Blur is sometimes used to hide content.
+    Assumptions:
+    - It is easier to unblur an element than find the root of such element and remove it.
+    - There is a very small chance of this effect being used for design, so all blur can be removed.
+    */
+    let filter = element_style['filter'];
 
-  let filter = element_style['filter'];
+    if (filter.includes('blur')) element.style.setProperty('filter', 'none', 'important');
+  },
 
-  if (filter.includes('blur')){
-    element.style.setProperty('filter', 'none', 'important');
-  }
-}
+  handle_transparency: function (element, element_style){
+    /*
+    Transparency is sometimes used to hide or diminish the visibility of content.
+    Assumptions:
+    - It is easier to make an element transparent than find the root of such element and remove it.
+    */
+    if (special_elements.excluded_for_transparency.includes(element.tagName)) return;
 
-function handle_transparency(element, element_style){
+    let bgcolor = element_style['background-color'];
 
-  let bgcolor = element_style['background-color'];
+    const match = bgcolor.match(/rgba\((?<R>\d+),\s*(?<G>\d+),\s*(?<B>\d+),\s*(?<A>\d|\d\.\d+)\)/i);
+    if (match && Number(match.groups['A']) !== 0){
+      element.style.setProperty('background-color', 'rgba(' + match.groups['R'] + ',' + match.groups['G'] + ',' + match.groups['B'] + ',0)', 'important');
+    }
+  },
 
-  const match = bgcolor.match(/rgba\((?<R>\d+),\s*(?<G>\d+),\s*(?<B>\d+),\s*(?<A>\d|\d\.\d+)\)/i);
-  if (match && Number(match.groups['A']) !== 0){
-    element.style.setProperty('background-color', 'rgba(' + match.groups['R'] + ',' + match.groups['G'] + ',' + match.groups['B'] + ',0)', 'important');
-  }
-}
+  handle_z_index: function (element, element_style){
+    /*
+    The "z-index" plays an enormous role in popups. Apparently most popups use it.
+    Assumptions:
+    - Some elements cant/extremely unlikely to be used as containers for popups.
+    */
+    if (special_elements.excluded_for_z_index.includes(element.tagName)) return;
 
-/* These elements are special and are highly unlikely to be a container for a popup.
-*/
-const special_z_index_elements = [
-  'HEADER', 'MAIN', 'SECTION', 'ARTICLE', 'ASIDE', 'NAV', 'FOOTER', 'FRAME', 'FRAMESET',
-  'P', 'A', 'BUTTON', 'CANVAS', 'FORM', 'H1', 'H2', 'H3', 'H4', 'IFRAME', 'SPAN',
-];
+    let z_index = element_style['z-index'];
+    if (z_index !== 'auto' && z_index !== '0' &&
+      support_for_css_properties_handlers.has_non_special_ancestors(element) &&
+      support_for_css_properties_handlers.has_non_special_descendants(element)
+    ){
+      remove_element(element); return 'removed';
+    }
+  },
+};
 
-function handle_z_index(element, element_style){
-  /* "z-index" plays an enormous role in popups. Apparently most popups use it.
+
+const support_for_css_properties_handlers = {
+  /*
+  Miscellaneous functions, which are used by the functions defined in the "css_properties_handlers".
   */
-  if (special_z_index_elements.includes(element.tagName)){
-    return null;
-  }
+  has_non_special_ancestors: function (element){
+    /*
+    The ancestors_for_z_index shall preserve their descendants.
+    Returns false if the given element has such special ancestor, otherwise true.
+    */
+    let current_ancestor = element.parentElement;
 
-  let z_index = element_style['z-index'];
-  const non_special_ancestor = has_non_special_ancestor(element);
+    while (current_ancestor != body && current_ancestor != html){
+      // It can happen that the ancestor is null.
+      if (!(current_ancestor instanceof HTMLElement)) return;
+      if (special_elements.ancestors_for_z_index.includes(current_ancestor.tagName)) return false;
+      current_ancestor = current_ancestor.parentElement;
+    }
+    return true;
+  },
 
-  if (z_index !== 'auto' && z_index !== '0' && non_special_ancestor){
-    remove_element(element);
-    return 'removed';
-  }
-}
+  has_non_special_descendants: function (root_element){
+    /*
+    The descendants_for_z_index shall preserve their ancestor.
+    Returns false if the given element has such special descendant, otherwise true.
+    */
+    let output = true;
 
-function has_non_special_ancestor(element){
-  /* The special_z_index_elements shall preserve their descendants.
-  Returns false if the given element has such special ancestor, otherwise true.
-  */
-  let current_ancestor = element.parentElement;
-
-  while (current_ancestor != body && current_ancestor != html){
-    // It can happen that the ancestor is null.
-    if (!(current_ancestor instanceof HTMLElement)){
+    function recurse_dom(element){
+      for (const _element of element.children){
+        if (!excluded_elements.includes(_element.tagName)){
+          if (special_elements.descendants_for_z_index.includes(_element.tagName)) output = false;
+          if (output) recurse_dom(_element);
+        }
+      }
+      if (output){
+      // It can happen that a part of a popup is a shadow DOM.
+        const shadow = element.shadowRoot;
+        if (shadow){
+          for (const _element of shadow.children){
+            if (!excluded_elements.includes(_element.tagName)){
+              if (special_elements.descendants_for_z_index.includes(_element.tagName)) output = false;
+              if (output) recurse_dom(_element);
+            }
+          }
+        }
+      }
       return;
     }
-    if (special_z_index_elements.includes(current_ancestor.tagName)){
-      return false;
-    }
-    current_ancestor = current_ancestor.parentElement;
+    recurse_dom(root_element);
+
+    return output;
   }
-  return true;
-}
+};
+
 
 function remove_element(element){
-  /* Safe removal of element.
-  */
-  if (body.contains(element)){
-    element.remove();
-  }
+  // Safe removal of element.
+  if (body.contains(element)) element.remove();
 }
 
 /*
@@ -171,30 +257,26 @@ function handle_special_sites(origin){
 
 // Elements, which cant be or extremely unlikely to be a part of any element-based annoyance.
 const excluded_elements = [
-  'HEAD', 'TITLE', 'META', 'LINK', 'BR', 'WBR', 'SCRIPT', 'NOSCRIPT', 'STYLE', 'ABBR',
-  'ACRONYM', 'ADDRESS', 'B', 'BDI', 'BDO', 'BIG', 'BLOCKQUOTE', 'CAPTION', 'CENTER', 'Q',
-  'CITE', 'CODE', 'TABLE', 'COL', 'COLGROUP', 'DATA', 'DATALIST', 'DL', 'DT', 'DD', 'I',
-  'DEL', 'DETAILS', 'DFN', 'EM', 'FIELDSET', 'LEGEND', 'INPUT', 'FIGURE', 'IMG', 'FONT',
-  'FIGCAPTION', 'FORM', 'HR', 'INS', 'KBD', 'LABEL', 'OL', 'UL', 'LI', 'MARK', 'MARQUEE',
-  'MENU', 'MENUITEM', 'MAP', 'AREA', 'METER', 'NOBR', 'SELECT', 'OPTGROUP', 'OPTION', 'P',
-  'OUTPUT', 'PARAM', 'PICTURE', 'SOURCE', 'PLAINTEXT', 'PRE', 'PROGRESS', 'RB', 'RP', 'RT',
-  'RTC', 'RUBY', 'S', 'SAMP', 'SEARCH', 'SMALL', 'STRIKE', 'STRONG', 'SUB', 'SUMMARY',
-  'SUP', 'TBODY', 'TD', 'TEXTAREA', 'TFOOT', 'TH', 'THEAD', 'TIME', 'TR', 'VIDEO', 'TRACK',
-  'AUDIO', 'TT', 'U', 'VAR', 'XMP',
+  'ABBR', 'ACRONYM', 'ADDRESS', 'AREA', 'AUDIO', 'B', 'BDI', 'BDO', 'BIG', 'BLOCKQUOTE', 'BR', 'CAPTION',
+  'CENTER', 'CITE', 'CODE', 'COL', 'COLGROUP', 'DATA', 'DATALIST', 'DD', 'DEL', 'DETAILS', 'DFN', 'DL',
+  'DT', 'EM', 'FIELDSET', 'FIGCAPTION', 'FIGURE', 'FONT', 'FORM', 'HEAD', 'HR', 'I', 'IMG', 'INPUT', 'INS',
+  'KBD', 'LABEL', 'LEGEND', 'LI', 'LINK', 'MAP', 'MARK', 'MARQUEE', 'MENU', 'MENUITEM', 'META', 'METER',
+  'NOBR', 'NOSCRIPT', 'OL', 'OPTGROUP', 'OPTION', 'OUTPUT', 'P', 'PARAM', 'PICTURE', 'PLAINTEXT', 'PRE',
+  'PROGRESS', 'Q', 'RB', 'RP', 'RT', 'RTC', 'RUBY', 'S', 'SAMP', 'SCRIPT', 'SEARCH', 'SELECT', 'SMALL',
+  'SOURCE', 'STRIKE', 'STRONG', 'STYLE', 'SUB', 'SUMMARY', 'SUP', 'TABLE', 'TBODY', 'TD', 'TEXTAREA',
+  'TFOOT', 'TH', 'THEAD', 'TIME', 'TITLE', 'TR', 'TRACK', 'TT', 'U', 'UL', 'VAR', 'VIDEO', 'WBR', 'XMP',
 ];
 
 function examine_and_handle_element(element){
 
   // Not an HTML element or excluded.
-  if (!(element instanceof HTMLElement) || excluded_elements.includes(element.tagName)){
-    return;
-  }
+  if (!(element instanceof HTMLElement) || excluded_elements.includes(element.tagName)) return;
 
   const style = getComputedStyle(element);
 
-  handle_blur(element, style);
-  handle_transparency(element, style);
-  handle_z_index(element, style);
+  css_properties_handlers.handle_blur(element, style);
+  //handle_transparency(element, style);
+  css_properties_handlers.handle_z_index(element, style);
 }
 
 function examine_dom(root_element){
@@ -227,14 +309,18 @@ function examine_dom(root_element){
 }
 
 function is_site_ignored(host){
+  /*
+  Check if the given host must be ignored as defined in the consts/sites.js
+  */
   if (STATIC_IGNORED_SITES.includes(host)) return true;
-  for (const rgx of RGX_IGNORED_SITES){
-    if (host.match(rgx)) return true;
-  }
+  for (const rgx of RGX_IGNORED_SITES) if (host.match(rgx)) return true;
   return false;
 }
 
 function is_site_special(host){
+  /*
+  Check if the given host must be handled in some special way as defined in the consts/sites.js
+  */
   if (STATIC_SPECIAL_SITES.includes(host)) return true;
   return false;
 }
@@ -246,51 +332,32 @@ chrome.runtime.onMessage.addListener((data, sender) => {
   */
   const mode = data.message.mode;
   if (mode === 'enabled'){
-    if (is_site_ignored(origin)){
-      // Skip, we dont want to modify such sites.
-    }
-    else {
-      // Handle special sites in some specific way.
-      if (is_site_special(origin)){
-        handle_special_sites(origin);
-      }
-      // Otherwise handle general-type sites according to some common pattern.
-      else {
-        // Options for the observer (which mutations to observe)
-        const html_config = { attributes: true, childList: true, subtree: true };
+    if (is_site_ignored(origin)) return;
 
-        const html_callback = (mutationList, observer) => {
-          for (const mutation of mutationList){
-            if (mutation.type === "childList"){
-              for (const element of mutation.addedNodes){
-                examine_and_handle_element(element);
-              }
-            }
-            else if (mutation.type === "attributes"){
-              if (mutation.target==html){
-                handle_overflow(html, html_style);
-              }
-              else if (mutation.target==body){
-                handle_overflow(body, body_style);
-              }
-              else {
-                examine_and_handle_element(mutation.target);
-              }
-            }
-          }
-        };
-        const html_observer = new MutationObserver(html_callback);
-        html_observer.observe(html, html_config);
+    if (is_site_special(origin)){ handle_special_sites(origin); return; }
+    /*
+    Otherwise handle general-type sites according to some common pattern. */
+    const html_config = { attributes: true, childList: true, subtree: true };
 
-        // Make sure that popups, which were created before the MutationObserver was started, are taken care of.
-        handle_overflow(html, html_style);
-        handle_overflow(body, body_style);
-        examine_dom(body);
+    const html_callback = (mutationList, observer) => {
+      for (const mutation of mutationList){
+        if (mutation.type === "childList"){
+          for (const element of mutation.addedNodes) examine_and_handle_element(element);
+        }
+        else if (mutation.type === "attributes"){
+          if (mutation.target==html) css_properties_handlers.handle_overflow(html, html_style);
+          else if (mutation.target==body) css_properties_handlers.handle_overflow(body, body_style);
+          else examine_and_handle_element(mutation.target);
+        }
       }
-    }
-  }
-  else {
-    // Mode is "disabled". Dont do anything.
+    };
+    const html_observer = new MutationObserver(html_callback);
+    html_observer.observe(html, html_config);
+
+    // Make sure that popups, which were created before the MutationObserver was started, are taken care of.
+    css_properties_handlers.handle_overflow(html, html_style);
+    css_properties_handlers.handle_overflow(body, body_style);
+    examine_dom(body);
   }
 });
 
